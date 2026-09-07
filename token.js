220862c16b7f375a24fdd4825dc59c7171250c03
Danzznil_xyz <rohmatinnur8@gmail.com>
2026-09-07 17:09:56 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8615008484:AAEtfjPvyDTM2fFZjD8bDuhB2PhJybyA5Hc"
+    "8615008484:AAEtfjPvyDTM2fFZjD8bDuhB2PhJybyA5Hc",
+    "8829323309:AAHchJNaloRdBnVKn8Sbk5M60aR9r-Qxgtg"
   ]
 }
