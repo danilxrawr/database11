9468c099180867c16b2bf403845494ba27963d6b
Danzznil_xyz <rohmatinnur8@gmail.com>
2026-09-07 21:35:41 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8615008484:AAEtfjPvyDTM2fFZjD8bDuhB2PhJybyA5Hc",
-    "8829323309:AAHchJNaloRdBnVKn8Sbk5M60aR9r-Qxgtg"
+    "8829323309:AAHchJNaloRdBnVKn8Sbk5M60aR9r-Qxgtg",
+    "8706101844:AAG-kuoomA3MtwN2YS1B2scD4lxo3b5nQLc"
   ]
 }
